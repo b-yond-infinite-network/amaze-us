5f0f3d6f812736a290afde6c02e9ab6a3a06981c
Musab Mirza <hellomusab@gmail.com>
2018-03-09 12:59:25 -0500

    added stub for save

--- challenge-1/frontend/src/main/java/com/challenge/frontend/controllers/WelcomeController.java
+++ challenge-1/frontend/src/main/java/com/challenge/frontend/controllers/WelcomeController.java
@@ -1,17 +1,37 @@
 package com.challenge.frontend.controllers;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class WelcomeController {
 	
 	@RequestMapping("/")
-	public String greeting(Model model) {
-		
-		
+	public String index(Model model) {
         return "index";
     }
+	
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(HttpServletRequest request, 
+	        @RequestParam(value="name") String name, 
+	        @RequestParam(value="email") String email, 
+	        @RequestParam(value="description") String description)
+	        		throws ServletException, IOException {
+		
+		// Todo
+		
+		return null;
+
+	}
 
 }
