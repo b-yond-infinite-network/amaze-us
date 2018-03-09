fb8479ba9d50f641ce86bd750ca7bf32949fea6c
Musab Mirza <hellomusab@gmail.com>
2018-03-09 12:01:47 -0500

    initial commit for frontend

---
+++ challenge-1/frontend/src/main/java/com/challenge/frontend/FrontendApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+}
