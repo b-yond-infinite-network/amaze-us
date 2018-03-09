27f1b0dff4a218f957e8fad6c195c9201bb19778
Musab Mirza <hellomusab@gmail.com>
2018-03-09 13:12:28 -0500

    added the error page

--- challenge-1/frontend/src/main/java/com/challenge/frontend/controllers/WelcomeController.java
+++ challenge-1/frontend/src/main/java/com/challenge/frontend/controllers/WelcomeController.java
@@ -1,8 +1,6 @@
 package com.challenge.frontend.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
