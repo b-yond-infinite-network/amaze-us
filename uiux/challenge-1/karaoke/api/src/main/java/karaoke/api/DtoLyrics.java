7da3f4e476e1804b00a1fe54c9e1878904f8df06
Thiago Veronezi <thiago@veronezi.org>
2018-04-13 14:34:40 -0300

    lombok

--- uiux/challenge-1/karaoke/api/src/main/java/karaoke/api/DtoLyrics.java
+++ uiux/challenge-1/karaoke/api/src/main/java/karaoke/api/DtoLyrics.java
@@ -1,47 +1,25 @@
 package karaoke.api;
 
-import java.util.Objects;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode(exclude = {"text", "track"})
 public class DtoLyrics {
+
+    @Getter
+    @Setter
     private Long id;
+
+    @Getter
+    @Setter
     private String text;
+
+    @Getter
+    @Setter
     private DtoTrack track;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public DtoTrack getTrack() {
-        return track;
-    }
-
-    public void setTrack(DtoTrack track) {
-        this.track = track;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DtoLyrics dtoLyrics = (DtoLyrics) o;
-        return Objects.equals(id, dtoLyrics.id);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(id);
-    }
 }
--- uiux/challenge-1/karaoke/api/src/main/java/karaoke/api/DtoTrack.java
+++ uiux/challenge-1/karaoke/api/src/main/java/karaoke/api/DtoTrack.java
@@ -1,56 +1,28 @@
 package karaoke.api;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode(exclude = {"title", "artist", "length"})
 public class DtoTrack {
+
+    @Getter
+    @Setter
     private Long id;
+
+    @Getter
+    @Setter
     private String title;
+
+    @Getter
+    @Setter
     private String artist;
+
+    @Getter
+    @Setter
     private Long length;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public Long getLength() {
-        return length;
-    }
-
-    public void setLength(Long length) {
-        this.length = length;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DtoTrack dtoTrack = (DtoTrack) o;
-        return Objects.equals(id, dtoTrack.id);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(id);
-    }
 }
