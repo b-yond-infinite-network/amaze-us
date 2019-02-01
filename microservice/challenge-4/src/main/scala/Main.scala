ce71dadae99e0922eb41bb40c784917e84fc8fef
laparant <laurie-anne.parant@uqac.ca>
2019-02-01 00:20:15 -0500

    Add single cat simulator
    Cat class has similar controllers as usual Thread implementation :
    1. Method live (usually run) to launch the loop of life of the cat.
    The loop is infinite while **alive** attribute is true.
    2. Method kill to stop live infinite loop, by setting the **alive**
    attribute to false.
    The current live function prints the current mood for the tests.
    The file Main.scala tests the good behavior of this minimal Cat.
    
    The mood is modelized as sealed trait to enable only legal moods :
    GROWL, HISS, PURR, THROWGLASS, ROLLONFLOOR, SCRATCHCHAIRS and
    LOOKDEEPINEYES.
    
    The chooseMood function of Cat selects randomly the new mood of the Cat.

---
+++ microservice/challenge-4/src/main/scala/Main.scala
@@ -0,0 +1,22 @@
+import simucat.Cat
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object Main extends App {
+  implicit val ec = ExecutionContext.global
+  val cat1 = new Cat()
+  Future {cat1.live()}(ec)
+
+  val cat2 = new Cat()
+  Future {cat2.live()}(ec)
+
+  Thread.sleep(30000*2)
+
+  cat1.kill()
+  println("Cat 1 killed")
+
+  Thread.sleep(30000*2)
+
+  cat2.kill()
+  println("Cat 2 killed")
+}
---
+++ microservice/challenge-4/src/main/scala/simucat/Cat.scala
@@ -0,0 +1,43 @@
+package simucat
+
+import scala.util.Random
+
+sealed trait Mood {
+  val sound : String
+}
+case object GROWL extends Mood {val sound = "grr"}
+case object HISS extends Mood {val sound = "kssss"}
+case object PURR extends Mood {val sound = "rrrrr"}
+case object THROWGLASS extends Mood {val sound = "cling bling"}
+case object ROLLONFLOOR extends Mood {val sound = "fffff"}
+case object SCRATCHCHAIRS extends Mood {val sound = "gratgrat"}
+case object LOOKDEEPINEYES extends Mood {val sound = "-o-o-___--"}
+
+class Cat {
+  var mood : Mood = chooseMood()
+  private var alive : Boolean = true
+
+  def chooseMood(): Mood = {
+    Random.nextInt(7) match {
+      case 0 => GROWL
+      case 1 => HISS
+      case 2 => PURR
+      case 3 => THROWGLASS
+      case 4 => ROLLONFLOOR
+      case 5 => SCRATCHCHAIRS
+      case 6 => LOOKDEEPINEYES
+    }
+  }
+
+  def live(): Unit = {
+    while (alive) {
+      mood = chooseMood()
+      println(s"$mood")
+      Thread.sleep(27000)
+    }
+  }
+
+  def kill(): Unit = {
+    alive = false
+  }
+}
