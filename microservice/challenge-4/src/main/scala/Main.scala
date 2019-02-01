c9870f9989f236567ddc1dc215641bbe82f267fd
laparant <laurie-anne.parant@uqac.ca>
2019-02-01 02:39:17 -0500

    Improve encapsulation and add comments
    Before background-ing the cat life cycle was done by the Cat instanciator
    with the **Future** keyword. Now the this work is done into the Cat.live()
    to better encapsulate this function. The user can't be responsible of this.
    
    Add comments to the Cat class in order to put into release branch.

--- microservice/challenge-4/src/main/scala/Main.scala
+++ microservice/challenge-4/src/main/scala/Main.scala
@@ -1,22 +1,20 @@
 import simucat.Cat
 
-import scala.concurrent.{ExecutionContext, Future}
-
 object Main extends App {
-  implicit val ec = ExecutionContext.global
   val cat1 = new Cat(1)
-  Future {cat1.live()}(ec)
+  cat1.live()
 
   val cat2 = new Cat(2)
-  Future {cat2.live()}(ec)
+  cat2.live()
 
-  Thread.sleep(30000*2)
+  Thread.sleep(30000 * 2)
 
   cat1.kill()
   println("Cat 1 killed")
 
-  Thread.sleep(30000*2)
+  Thread.sleep(30000 * 2)
 
   cat2.kill()
   println("Cat 2 killed")
+
 }
--- microservice/challenge-4/src/main/scala/simucat/Cat.scala
+++ microservice/challenge-4/src/main/scala/simucat/Cat.scala
@@ -1,7 +1,12 @@
 package simucat
 
 import scala.util.Random
+import scala.concurrent.{ExecutionContext, Future}
 
+/** Available moods for the Cat
+  *
+  * @attribute sound : phonic representation of the mood
+  */
 sealed trait Mood {
   val sound : String
 }
@@ -13,11 +18,22 @@ case object ROLLONFLOOR extends Mood {val sound = "fffff"}
 case object SCRATCHCHAIRS extends Mood {val sound = "gratgrat"}
 case object LOOKDEEPINEYES extends Mood {val sound = "-o-o-___--"}
 
+/** A simulated cat with a mood
+  *
+  * @constructor create a new cat with an ID and a randomly selected mood
+  * @param catID : the cat ID
+  * @attribute mood : the cat mood (see trait Mood)
+  * @attribute alive : Boolean indicating if the Cat is alive or not
+  */
 class Cat(catID : Int) {
   private val id : Int = catID
   private var mood : Mood = chooseMood()
   private var alive : Boolean = true
 
+  /** Randomly choose a mood among the legal ones
+    *
+    * @return randomly chosen mood
+    */
   def chooseMood(): Mood = {
     Random.nextInt(7) match {
       case 0 => GROWL
@@ -30,14 +46,24 @@ class Cat(catID : Int) {
     }
   }
 
+  /** Life cycle of the cat
+    * Each 27 seconds, the cat changes its mood.
+    * The loop is infinite until the kill() call.
+    */
   def live(): Unit = {
-    while (alive) {
-      mood = chooseMood()
-      println(s"Cat $id : ${mood.sound}")
-      Thread.sleep(27000)
-    }
+    implicit val ec = ExecutionContext.global
+    Future {
+      while (alive) {
+        mood = chooseMood()
+        println(s"Cat $id : ${mood.sound}")
+        Thread.sleep(27000)
+      }
+    }(ec)
   }
 
+  /** End the life cycle of the cat
+    * Set alive attribute to false to end loop in live()
+    */
   def kill(): Unit = {
     alive = false
   }
