d08497abe904c9886314e2d6496741f7cd86cfb1
amir <am1hosseinzadeh@gmail.com>
2019-03-30 17:44:27 -0400

    fix db connection error

--- microservice/challenge-1/app42/backend/server.ts
+++ microservice/challenge-1/app42/backend/server.ts
@@ -13,14 +13,14 @@ logger.level = "debug";
 app.set("port", config.server.port);
 const server = http.createServer(app);
 
-db.sync({force: true}).then(() => {
+db.sync().then(() => {
     logger.info("[i] database connected");
     server.listen(config.server.port);
     server.on("error", (error) => {
         logger.error("[X] " + error);
     });
     server.on("listening", () => logger.info(`BACKEND is listening on port ${config.server.port}`));
-}).catch((error) => logger.error("[X] error in postgres connection", error));
+}).catch((error) => logger.error("[X] error in database connection", error));
 
 process.on("SIGINT", () => {
     logger.info("Stopping BACKEND.");
--- microservice/challenge-1/app42/backend/src/database/DbConnection.ts
+++ microservice/challenge-1/app42/backend/src/database/DbConnection.ts
@@ -1,12 +1,11 @@
 import {Sequelize} from "sequelize-typescript";
 
-const sequelize =  new Sequelize({
+const sequelize = new Sequelize({
     database: "byond",
     dialect: "mysql",
-    host: "mysql1",
-    modelPaths: [__dirname + "../entity"],
+    host: "mysql",
+    modelPaths: [__dirname + "/../entity/"],
     password: "root",
     username: "root",
 });
-
 export default sequelize;
