530205cf513b2060e5bf42427c50291e4d18a298
amir <am1hosseinzadeh@gmail.com>
2019-04-01 09:20:20 -0400

    add react

---
+++ microservice/challenge-1/app42/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <p>test</p>
+        </React.Fragment>
+      </Router>
+    );
+  }
+}
+
+export default App;
