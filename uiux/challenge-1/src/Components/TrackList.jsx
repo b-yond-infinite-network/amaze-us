d9eee604c9cc7d9150e088342d95024ea8f13e5f
Garo Assilan <garo.assilian.work@gmail.com>
2019-07-18 19:35:17 -0400

    Started using React Hooks. Really cool.

--- uiux/challenge-1/src/Components/TrackList.jsx
+++ uiux/challenge-1/src/Components/TrackList.jsx
@@ -1,45 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 import { Row, Col } from 'reactstrap';
 import TrackRow from './TrackRow';
 
-export default class TrackList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTrackId: undefined,
-    }
-  }
+const  TrackList = (props) => {
 
-  changeSelectedTrack = (newTrack) => {
-    const { selectedTrackId } = this.state;
-    this.setState({
-      selectedTrackId: newTrack === selectedTrackId ? undefined : newTrack
-    })
+  const [selectedTrackId, setSelectedTrack] = useState(undefined);
+
+  const changeSelectedTrack = (newTrack) => {
+    setSelectedTrack(newTrack === selectedTrackId ? undefined : newTrack)
   } 
 
-  render() {
-    const { tracks, changeSortAttribute, sortedBy } = this.props;
-    const { selectedTrackId } = this.state;
-    return (
-      <div className="container">
-        <Row>
-          <Col className={`header-col ${sortedBy === 'track_name' ? 'selected' : ''}`} onClick={() => changeSortAttribute('track_name')}>Track Title</Col>
-          <Col className={`header-col ${sortedBy === 'artist_name' ? 'selected' : ''}`} onClick={() => changeSortAttribute('artist_name')}>Artist</Col>
-          <Col className={`header-col ${sortedBy === 'wordCount' ? 'selected' : ''}`}  onClick={() => changeSortAttribute('wordCount')}>Lyrics Length</Col>
-        </Row>
-        <hr />
-        {tracks.map(track => (
-          <TrackRow 
-            track={track}
-            key={track.track_id}
-            onClick={this.changeSelectedTrack}
-            showLyrics={selectedTrackId === track.track_id} 
-          />
-        ))}
-      </div>
-    )
-  }
+  const { tracks, changeSortAttribute, sortedBy } = props;
+  return (
+    <div className="container">
+      <Row>
+        <Col className={`header-col ${sortedBy === 'track_name' ? 'selected' : ''}`} onClick={() => changeSortAttribute('track_name')}>Track Title</Col>
+        <Col className={`header-col ${sortedBy === 'artist_name' ? 'selected' : ''}`} onClick={() => changeSortAttribute('artist_name')}>Artist</Col>
+        <Col className={`header-col ${sortedBy === 'wordCount' ? 'selected' : ''}`}  onClick={() => changeSortAttribute('wordCount')}>Lyrics Length</Col>
+      </Row>
+      <hr />
+      {tracks.map(track => (
+        <TrackRow 
+          track={track}
+          key={track.track_id}
+          onClick={changeSelectedTrack}
+          showLyrics={selectedTrackId === track.track_id} 
+        />
+      ))}
+    </div>
+  )
 }
 
 TrackList.propTypes = {
@@ -56,3 +46,5 @@ TrackList.propTypes = {
 TrackList.defaultProps = {
   tracks: [],
 }
+
+export default TrackList;
