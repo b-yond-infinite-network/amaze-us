338985b5a0414f1dcba22b592d1d89f230d48213
Roberto Aguilar <raguilar@devlab.local>
2019-07-23 20:16:36 -0600

    + Adding md5 for search values hashing

--- uiux/challenge-1/amaze-us/src/app/musixmatch.service.ts
+++ uiux/challenge-1/amaze-us/src/app/musixmatch.service.ts
@@ -3,29 +3,30 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { pluck } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
+import { Md5 } from 'ts-md5/dist/md5';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MusixmatchService {
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     this.results$.subscribe(data => console.log(data));
   }
 
   results$ = new Subject();
 
-  subscribe(subscriber){
+  subscribe(subscriber) {
     return this.results$.subscribe(subscriber);
   }
 
-  search(term:string = '') {
+  search(term: string = '') {
 
-    if(!term || term === ''){
+    if (!term || term === '') {
       return;
-    }   
+    }
 
     const API_KEY = 'fcf949768093a50b3c8603f003b6d3ea';
-        
     let url = `http://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q=${term}&s_track_rating=desc&apikey=${API_KEY}`;
 
     this.http.jsonp(url, 'callback').pipe(
@@ -36,5 +37,16 @@ export class MusixmatchService {
       },
       (e: HttpErrorResponse) => console.log(e)
     );
+  }
+
+  private isTermInCache(term:string = ''){
+
+    if(!term || term === ''){
+      return false;
+    }
+
+    let termHash = Md5.hashStr(term.toLowerCase());
+
+    return true;
   }
 }
