1b3ee128e6e50d5766cdfc9685fe3a5978b3a3db
Igor Tcherniavski <igor.tcherniavski@bnc.ca>
2019-07-29 18:20:39 -0400

    added flyway migrations

---
+++ microservice/challenge-1/backend/bin/main/db/migration/V1__init.sql
@@ -0,0 +1,46 @@
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE users (
+    id bigint NOT NULL,
+    name character varying(150) NOT NULL,
+    email character varying(150) NOT NULL,
+    description character varying(250)
+);
+
+
+ALTER TABLE users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
---
+++ microservice/challenge-1/backend/src/main/resources/db/migration/V2__insert_data.sql
@@ -0,0 +1,7 @@
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO users VALUES (1, 'Igor', 'igor@gmail.com', 'first user in');
+INSERT INTO users VALUES (2, 'integration test', 'test@gmal.com', 'Used for integration testing, do not delete');
+INSERT INTO users VALUES (3, 'Jack', 'jack@gmail.com', 'third user in');
