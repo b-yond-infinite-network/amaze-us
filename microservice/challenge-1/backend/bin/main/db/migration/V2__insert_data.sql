bb014d0996492c38ad724d46e474daf5b741f5c0
Igor Tcherniavski <igor.tcherniavski@bnc.ca>
2019-07-29 20:11:26 -0400

    upgraded ci-cd to tag docker properly and always trigger k8s rollout

---
+++ microservice/challenge-1/backend/bin/main/db/migration/V2__insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users VALUES (default, 'Igor', 'igor@gmail.com', 'first user in');
+INSERT INTO users VALUES (default, 'integration test', 'test@gmal.com', 'Used for integration testing, do not delete');
+INSERT INTO users VALUES (default, 'Jack', 'jack@gmail.com', 'third user in');
