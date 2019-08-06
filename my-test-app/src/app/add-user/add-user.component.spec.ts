347f846d96e20454f7a025fb6c32593c353e2a10
Piyali Das <official88piyali@gmail.com>
2019-08-06 16:14:11 +0530

    refactoring UI test cases

--- my-test-app/src/app/add-user/add-user.component.spec.ts
+++ my-test-app/src/app/add-user/add-user.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddUserComponent } from './add-user.component';
 import { Router } from '@angular/router';
 import { UsersService } from '../service/users.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AddUserComponent', () => {
   let component: AddUserComponent;
@@ -19,18 +20,32 @@ describe('AddUserComponent', () => {
       providers: [
         {provide: UsersService, useValue: usersServiceSpy},
         {provide: Router, useValue: routerSpy}
-      ]
+      ],
+      imports [ ReactiveFormsModule ]
     })
     .compileComponents();
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    usersService = TestBed.get(UsersService);
   });
 
-  it('should create', () => {
+  it('should submit user form', () => {
     expect(component).toBeTruthy();
+    component.userForm.name = 'name';
+    component.userForm.email = 'email@mail.com';
+    component.userForm.description = 'some description';
+
+    fixture.detectChanges();
+
+    const el = fixture.nativeElement.querySelector('button');
+    el.click();
+    fixture.detectChanges();
+    expect(usersService.addUser.calls.count()).toBe(1, 'one call');
   });
+
 });
--- my-test-app/src/app/view-users/view-users.component.spec.ts
+++ my-test-app/src/app/view-users/view-users.component.spec.ts
@@ -2,24 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewUsersComponent } from './view-users.component';
 import { UsersService } from '../service/users.service';
-import { defer } from 'rxjs';
+import { of, throwError, Observable } from 'rxjs';
 import { DebugElement } from "@angular/core";
 import { By } from "@angular/platform-browser";
-
-
-export function asyncDataResolved<T>(data: T) {
-    return defer(() => Promise.resolve(data));
-}
-
-export function asyncDataRejected<T>(data: T) {
-    return defer(() => Promise.reject(data));
-}
 
 describe('ViewUsersComponent', () => {
   let component: ViewUsersComponent;
   let fixture: ComponentFixture<ViewUsersComponent>;
   let usersService : jasmine.SpyObj<UsersService>;
-  let el: DebugElement;
 
   beforeEach(() => {
     const usersServiceSpy = jasmine.createSpyObj('UsersService', ['getUsers']);
@@ -42,41 +32,40 @@ describe('ViewUsersComponent', () => {
             { name:'user2', email:'email2@mail.com', description:'desc2' }
         ];
 
-    usersService.getUsers.and.returnValue(asyncDataResolved(expectedUsers));
+    usersService.getUsers.and.returnValue(of(expectedUsers));
     fixture = TestBed.createComponent(ViewUsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
 
     expect(component).toBeTruthy();
-
-    el = fixture.debugElement.query(By.css('tr'));
-    expect(el.nativeElement.length).toBe(3);
+    const el = fixture.nativeElement.querySelectorAll('tr');
+    expect(el.length).toBe(3);
   });
 
   it('should show no users available', () => {
     const expectedUsers: Array<User> = [];
 
-    usersService.getUsers.and.returnValue(asyncDataResolved(expectedUsers));
+    usersService.getUsers.and.returnValue(of(expectedUsers));
     fixture = TestBed.createComponent(ViewUsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
 
     expect(component).toBeTruthy();
-    el = fixture.debugElement.query(By.css('h1'));
-    expect(el.nativeElement.textContent).toEqual('No data');
+    const el = fixture.nativeElement.querySelector('h1');
+    expect(el.textContent).toEqual('No data');
   });
 
   it('should show errors', () => {
-    const expectedUsers: Array<User> = [];
+    const error: Error = new Error('test error');
 
-    usersService.getUsers.and.returnValue(asyncDataRejected(expectedUsers));
+    usersService.getUsers.and.returnValue(throwError(error));
     fixture = TestBed.createComponent(ViewUsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
 
     expect(component).toBeTruthy();
-    el = fixture.debugElement.query(By.css('h2'));
-    expect(el.nativeElement.textContent).toEqual('Error occured. See Logs.');
+    const el = fixture.nativeElement.querySelector('h2');
+    expect(el.textContent).toEqual('Error occured. See Logs.');
   });
 
 });
