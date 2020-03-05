c995bacc0fa2caeaa4b0542c5286737d08d458c1
Vishnu Dhanabalan <vishnudhanabalan@outlook.com>
2020-03-05 23:12:28 +0530

    tests for /api/search/artist/

---
+++ uiux/challenge-1/backend/tests/api/search/artist/index.ts
@@ -0,0 +1,34 @@
+import { Server } from "http";
+import chai, { assert, expect } from "chai";
+import chaiHTTP from "chai-http";
+
+chai.use(chaiHTTP);
+
+export default (app: Server) => {
+  describe("testing /api/search/artist", () => {
+    it("Should return 200 status and response body array, when a valid request is made", done => {
+      chai
+        .request(app)
+        .get("/api/search/artist/")
+        .query({ name: "justin", page: 1, pageSize: 10 })
+        .end((err, res) => {
+          expect(res.status).to.be.equal(200);
+          assert.isArray(res.body, "Response body should be an array");
+          expect(res.body.length).to.be.equal(10);
+        });
+      done();
+    });
+
+    it("Should return 400 status with error message, when an invalid query parameter request is made", done => {
+      chai
+        .request(app)
+        .get("/api/search/artist/")
+        .query({ name: "justin" })
+        .end((err, res) => {
+          expect(res.status).to.be.equal(400);
+          assert.isObject(res.body, "Response body should be an object");
+        });
+      done();
+    });
+  });
+};
