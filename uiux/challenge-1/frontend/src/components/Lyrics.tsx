66ff756fd6c53a32d5d3d7ebbd60f40117844b7b
Vishnu Dhanabalan <vishnudhanabalan@outlook.com>
2020-03-13 15:15:43 +0530

    removed unused back to search option and related test statement changes

--- uiux/challenge-1/frontend/src/components/Lyrics.tsx
+++ uiux/challenge-1/frontend/src/components/Lyrics.tsx
@@ -7,7 +7,7 @@ import { ILyrics } from "../../../shared";
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 75%;
+  margin: 25px 0;
 `;
 
 const LyricsContent = styled.div`
@@ -16,16 +16,8 @@ const LyricsContent = styled.div`
   color: ${colorCodes.deepMatteGrey};
 `;
 
-const BackToSearch = styled.div`
-  font-family: BitterItalic;
-  font-size: 15px;
-  text-align: center;
-  cursor: pointer;
-`;
-
 Wrapper.displayName = "Wrapper";
 LyricsContent.displayName = "LyricsContent";
-BackToSearch.displayName = "BackToSearch";
 
 interface LyricsProps extends ILyrics {
   handleClose: Function;
@@ -35,13 +27,6 @@ const Lyrics: React.FC<LyricsProps> = (props: LyricsProps) => {
   return (
     <Wrapper>
       <LyricsContent>{props.lyricsContent}</LyricsContent>;
-      <BackToSearch
-        onClick={() => {
-          props.handleClose();
-        }}
-      >
-        back to search
-      </BackToSearch>
     </Wrapper>
   );
 };
