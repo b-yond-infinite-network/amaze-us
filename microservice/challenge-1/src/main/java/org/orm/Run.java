590568115f5fbca86957c8908eb7f90a6af3b57b
ali <aliakbar.dandach@isae.edu.lb>
2021-06-27 17:18:05 +0300

    refactoring #2

---
+++ microservice/challenge-1/src/main/java/org/orm/Run.java
@@ -0,0 +1,11 @@
+package org.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class);
+    }
+}
