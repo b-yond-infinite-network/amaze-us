7e8ec64579d472b8e44120bc6c0b66a14feb7ae1
Abdoulaye Sow <bigsow92@gmail.com>
2021-09-26 02:08:34 -0400

    Boilerplate - front and backend skeletons

---
+++ uiux/challenge-2/api-domy-neon-v2/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import log4js from 'log4js';
+
+const app = express();
+const logger = log4js.getLogger('Domy-Neon');
+logger.level = process.env.LOG_LEVEL || 'info';
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  logger.info(`Express server running at 0.0.0.0:${port}`);
+});
+
+app.use((_, res, __) => {
+  res.send('Hello!');
+});
