cfbb1917e0ac0d07f2113f6458fd3c3695cf40f2
Henrique Uzêda <hruzeda@gmail.com>
2022-09-06 03:08:58 -0300

    First commit - initial proposal for frontend/backend project structure

---
+++ advanced/challenge-1/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+require('dotenv').config({ path: 'env' })
+const devMode = process.env.NODE_ENV !== 'production'
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'public', 'index.html')
+  }),
+  new MiniCssExtractPlugin()
+]
+
+if (devMode) {
+  plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: devMode ? 'eval-source-map' : false,
+  plugins: plugins
+}
